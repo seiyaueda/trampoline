f310c34692696b6f5c35da91e6af83e470d49c25
上田聖也 <seiya7922@icloud.com>
2016-06-11 15:50:06 +0900

    はじめてーの、コミット♪

---
+++ Assets/LineGenerater.cs
@@ -0,0 +1,56 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class LineGenerater : MonoBehaviour {
+	[SerializeField]
+	bool isPressed;
+	
+	Vector3 startPressPosition;
+	Vector3 endPressPosition;
+	
+	public GameObject lineObject;
+	
+	// Use this for initialization
+	void Start () {
+		isPressed =false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Vector3 tempPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,9.9f);
+			startPressPosition = Camera.main.ScreenToWorldPoint(tempPosition);
+			Instantiate(lineObject,startPressPosition,Quaternion.identity);
+			
+			Debug.Log(startPressPosition);
+			
+			isPressed = true;
+		}
+		
+		if(Input.GetMouseButton(0)){
+			
+		}
+		
+		if(Input.GetMouseButtonUp(0)){
+			Vector3 tempPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,9.9f );
+			endPressPosition = Camera.main.ScreenToWorldPoint(tempPosition);
+			
+			generateLine();
+			
+			isPressed = false;
+		}
+		
+	}
+	
+	//リアルタイムで描画しない場合
+	void generateLine(){
+		
+		//Instantiate
+		//enabled = false
+		//SetVertexCount = 2
+		//setPositions
+		//enabled = true
+		
+		
+	}
+}
---
+++ Assets/LineObject.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineObject : MonoBehaviour {
+	Vector3 endPressPosition;
+	Vector3 nowPressPosition;
+	
+	LineRenderer lineRenderer;
+	public bool isPressed;
+	
+	// Use this for initialization
+	void Start () {
+		lineRenderer = this.GetComponent<LineRenderer>();
+		lineRenderer.SetVertexCount(2);
+		lineRenderer.enabled = false;
+		lineRenderer.SetPosition(0,transform.position);
+		
+		
+		isPressed = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetMouseButton(0) && isPressed){
+			Vector3 tempPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,9.9f);
+			nowPressPosition = Camera.main.ScreenToWorldPoint(tempPosition);
+			lineRenderer.SetPosition(1,nowPressPosition);
+			lineRenderer.enabled = true;
+			
+			
+		}
+		
+		if(Input.GetMouseButtonUp(0)){
+			Vector3 tempPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,9.9f );
+			endPressPosition = Camera.main.ScreenToWorldPoint(tempPosition);
+			Debug.Log(endPressPosition);
+			
+			isPressed = false;
+			
+		}
+		
+	}
+}
