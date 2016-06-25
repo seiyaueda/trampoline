48487ab397eef78fe43aec589e116cd1c26e02a0
上田聖也 <seiya7922@icloud.com>
2016-06-25 17:37:57 +0900

    ゴール判定付与 ゲームオーバー スコア追加 囲った

--- Assets/scripts/LineObject.cs
+++ Assets/scripts/LineObject.cs
@@ -7,6 +7,8 @@ public class LineObject : MonoBehaviour {
 	
 	LineRenderer lineRenderer;
 	public bool isPressed;
+
+	EdgeCollider2D edgeCollider;
 	
 	// Use this for initialization
 	void Start () {
@@ -14,9 +16,12 @@ public class LineObject : MonoBehaviour {
 		lineRenderer.SetVertexCount(2);
 		lineRenderer.enabled = false;
 		lineRenderer.SetPosition(0,transform.position);
-		
+
+		edgeCollider = this.GetComponent<EdgeCollider2D>();
 		
 		isPressed = true;
+
+		//Destroy(gameObject, 5.0f);
 	}
 	
 	// Update is called once per frame
@@ -28,13 +33,15 @@ public class LineObject : MonoBehaviour {
 			lineRenderer.SetPosition(1,nowPressPosition);
 			lineRenderer.enabled = true;
 			
-			
+			Vector2 tempColposition = (Vector2)nowPressPosition - (Vector2)transform.position;
+			edgeCollider.points = new Vector2[] {new Vector2(0, 0), tempColposition};
+
 		}
 		
 		if(Input.GetMouseButtonUp(0)){
 			Vector3 tempPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,9.9f );
 			endPressPosition = Camera.main.ScreenToWorldPoint(tempPosition);
-			Debug.Log(endPressPosition);
+			//Debug.Log(endPressPosition);
 			
 			isPressed = false;
 			
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public GameObject goalObject;
+	public float jamp;
+	public Rigidbody2D rb;
+	Vector2 jumpVec;
+	int score;
+	public GameObject ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		jumpVec = new Vector2(300f,300f);
+
+		rb.AddForce(jumpVec);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y <= -5){
+			Debug.Log("Gameover");
+		}
+			
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if(collision.gameObject.tag == "Goal"){
+			Debug.Log("Goal");
+			score += 100;
+			Debug.Log(score);
+			ScoreText.GetComponent<Text>().text = score.ToString();
+			/*Destroy(collision.gameObject);
+			Vector3 goalPosition = new Vector3(Random.Range(-2,2), Random.Range(-2,2), 0);
+			Instantiate(goalObject, goalPosition, Quaternion.identity);*/
+		}
+	}
+}
