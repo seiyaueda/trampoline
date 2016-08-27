1a99035299520d219acc59d0d927bf17f3df6937
上田聖也 <seiya7922@icloud.com>
2016-08-27 16:59:32 +0900

    UIのデザイン変更 ボールの射出角度ランダム化

--- Assets/scripts/GoalScore.cs
+++ Assets/scripts/GoalScore.cs
@@ -8,13 +8,20 @@ public class GoalScore : MonoBehaviour {
 	int goalScore;
 	int goalflag;
 	public GameObject ScoreText;
+	public Text HighScoreText;
+
+	private int HighScore;
+	private string key = "HIGH SCORE";
 
 	// Use this for initialization
 	void Start () {
 		score = 0;
-	
+		HighScore = PlayerPrefs.GetInt(key, 0);
+		HighScoreText.text = "HighScore: " + HighScore.ToString();
+		//HighScoreText.GetComponent<Text>().text = "HighScore: " + HighScore.ToString();
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (goalScore == 1000){
@@ -30,7 +37,7 @@ public class GoalScore : MonoBehaviour {
 				goal.transform.position = new Vector3 (-2.6f, -4.0f, -1.0f);
 			} 
 		}
-		
+
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
@@ -39,19 +46,27 @@ public class GoalScore : MonoBehaviour {
 			score += 100;
 			goalScore += 100;
 			Debug.Log(score);
-			ScoreText.GetComponent<Text>().text = score.ToString();
+			//ScoreText.text = "Score: " + score.ToString();
+			ScoreText.GetComponent<Text>().text = "Score: " + score.ToString();
 			/*Destroy(collision.gameObject);
-			Vector3 goalPosition = new Vector3(Random.Range(-2,2), Random.Range(-2,2), 0);
-			Instantiate(goalObject, goalPosition, Quaternion.identity);*/
+            Vector3 goalPosition = new Vector3(Random.Range(-2,2), Random.Range(-2,2), 0);
+            Instantiate(goalObject, goalPosition, Quaternion.identity);*/
+
+			if (score > HighScore) {
+				HighScore = score;
+				PlayerPrefs.SetInt(key, HighScore);
+				HighScoreText.text = "HighScore: " + HighScore.ToString();
+				//HighScoreText.GetComponent<Text>().text = "HighScore: " + HighScore.ToString();
+			}
 		}
-		
-			
-			//score += 100;
-			//Debug.Log(score);
-			//ScoreText.GetComponent<Text>().text = score.ToString();
-			/*Destroy(collision.gameObject);
-			Vector3 goalPosition = new Vector3(Random.Range(-2,2), Random.Range(-2,2), 0);
-			Instantiate(goalObject, goalPosition, Quaternion.identity);*/
+
+
+		//score += 100;
+		//Debug.Log(score);
+		//ScoreText.GetComponent<Text>().text = score.ToString();
+		/*Destroy(collision.gameObject);
+            Vector3 goalPosition = new Vector3(Random.Range(-2,2), Random.Range(-2,2), 0);
+            Instantiate(goalObject, goalPosition, Quaternion.identity);*/
 	}
 
 }
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	/*public GameObject goalObject;
-	int score;
-	public GameObject ScoreText;*/
+    int score;
+    public GameObject ScoreText;*/
 	public float jamp;
 	public Rigidbody2D rb;
 	Vector2 jumpVec;
@@ -14,35 +14,35 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-		jumpVec = new Vector2(100f,100f);
+		jumpVec = new Vector2(/*aa100f,100f*/Random.Range (50f, 130f),Random.Range (70f, 130f));
 
 		rb.AddForce(jumpVec);
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(transform.position.y <= -5){
-			
+
 			//Debug.Log("Gameover");
 			Destroy(gameObject);
 
 		}
 
 
-			
-	
+
+
 	}
 
 	/*void OnCollisionEnter2D(Collision2D collision){
-		if(collision.gameObject.tag == "Goal"){
-			Debug.Log("Goal");
-			score += 100;
-			Debug.Log(score);
-			ScoreText.GetComponent<Text>().text = score.ToString();
-			//Destroy(collision.gameObject);
-			//Vector3 goalPosition = new Vector3(Random.Range(-2,2), Random.Range(-2,2), 0);
-			//Instantiate(goalObject, goalPosition, Quaternion.identity);
-		}
-	}*/
+        if(collision.gameObject.tag == "Goal"){
+            Debug.Log("Goal");
+            score += 100;
+            Debug.Log(score);
+            ScoreText.GetComponent<Text>().text = score.ToString();
+            //Destroy(collision.gameObject);
+            //Vector3 goalPosition = new Vector3(Random.Range(-2,2), Random.Range(-2,2), 0);
+            //Instantiate(goalObject, goalPosition, Quaternion.identity);
+        }
+    }*/
 }
